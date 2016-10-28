71ad76a2b3d908f738948075ed90b4b0daa34c9f
momozqy <532676071@qq.com>
2016-10-28 16:53:46 +0800

    找媳妇儿重要，先暂停

--- src/com/xiaoyu/service/imp/HelloServiceImp.java
+++ src/com/xiaoyu/service/imp/HelloServiceImp.java
@@ -1,23 +1,35 @@
 package com.xiaoyu.service.imp;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
 import com.xiaoyu.mapper.PersonMapper;
 import com.xiaoyu.model.Person;
 import com.xiaoyu.service.HelloService;
 
+@RestController
 public class HelloServiceImp implements HelloService {
-	private PersonMapper personMapper;
+	@Autowired
+	PersonMapper personMapper;
+
+	@RequestMapping(value = "/user/", method = RequestMethod.GET)
+	public ResponseEntity<List<Person>> listAllPerson() {
+		/*
+		 * List<User> users = userService.findAllUsers(); if (users.isEmpty()) {
+		 * return new ResponseEntity<List<User>>(HttpStatus.NO_CONTENT);// You
+		 * // many // decide // to // return // HttpStatus.NOT_FOUND }
+		 */
+		return null;
+	}
 
 	@Override
 	public boolean checkUser(Person p) {
 
 		return false;
-	}
-
-	public PersonMapper getPersonMapper() {
-		return personMapper;
-	}
-
-	public void setPersonMapper(PersonMapper personMapper) {
-		this.personMapper = personMapper;
 	}
 }
